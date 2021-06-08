6573ee9d9dea3a77ccb8ced0857889b461dcdb68
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-06-08 11:31:01 +0530

    Deleted one file

--- main.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-y = np.array([50, 20, 10, 15, 5])
-my_labels = ["Bitcoin", "Ethereum", "Dogecoin", "Binance coin", 'XRP']
-
-plt.pie(y, labels = my_labels)
-plt.show()
